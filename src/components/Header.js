546c1c6b869b54cd0af6a578ead45fde553b1f2a
Alex <alexxlindsay808@hotmail.com>
2025-07-26 06:45:15 -0500

    Building hero banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
-import React from 'react';
+// import { useEffect, useRef } from "react";
 import { Link } from 'react-router-dom';
 import { Nav } from './Nav.js';
 import Logo from '../assets/img/logo.svg';
+// import { gsap } from 'gsap';
 
 export function Header() {
+
+
   return (
 
     <header>
-        <div className="max-w-7xl mx-auto flex justify-between items-center py-4 px-6">
+        <div className="flex justify-between items-center py-4 px-[4rem]">
             <Link to="/">
                 <img className="w-56 h-auto" src={Logo} alt="Green Root Logo" />
             </Link>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import heroImg from '../assets/img/healthy-chicken-bowl.png';
-// import { gsap } from 'gsap';
 
 export default function Home() {
   return (
-    <div className='flex hero -z-1 relative -translate-y-23'>
-      <div className='flex-1 bg-brand-secondary'>
-        <div className='pl-20'>
-          <div>
+    <div className='hero'>
+      <div className='max-w-7xl mx-auto'>
+
+        <div className='px-6 banner-text relative translate-y-[6rem]'>
+          <div className='absolute -left-[19rem] rotate-90 w-[25rem] top-[8.2rem] border-l-[11px] border-brand-primary pl-8'>
             <h2>Welcome To</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
               Faucibus in libero  risus risus risus risus.  risus risus 
               risus risus risus risus risus.
             </p>
           </div>
-          <div>
+
+          <div className='heading-text'>
             <h1>
               <span>Redefined</span>
               <span>Health</span>
             </h1>
           </div>
         </div>
+
       </div>
-      <div className='flex-1 bg-brand-grey'>
-        <img src={heroImg} alt="Chicken Bowl" />
+
+      <div className='flex relative -z-1 translate-y-[-27rem]'>
+        <div className='flex-1 bg-brand-secondary'></div>
+        <div className='flex-1 bg-brand-grey banner-img overflow-hidden'>
+          <img src={heroImg} alt="Chicken Bowl" />
+        </div>
       </div>
+     
     </div>
   );
 }
